65d5cc03fc3dd058899f306291d9ad5f35e632d5
ma-xinjing <maxj29@mail2.sysu.edu.cn>
2024-07-20 12:30:11 +0800

    new file:   chat.js 	new file:   img3.jpg 	modified:   style.css 	modified:   wechat.html

---
+++ chat.js
@@ -0,0 +1,37 @@
+
+function $(id){
+    return typeof id=="string"?document.getElementById(id):id;
+}
+
+window.onload = function(){
+    var titleName = $("chatlistbox").getElementsByTagName("li");
+    var tabcontent = $("rightSide_item").getElementsByTagName("div");
+    var parentDiv = $("rightSide_item");
+    var outerDivs = [];
+    if(parentDiv)
+    {
+        
+        var children = parentDiv.childNodes;
+        var outerDiv = parentDiv.querySelectorAll(":scope > div");
+        for (var i = 0; i < children.length; i++) {
+            if (children[i].nodeType === 1 && children[i].tagName.toLowerCase() === "div") {
+                outerDivs.push(children[i]);
+            }
+        }
+    }
+    // alert(outerDivs.length);
+    for(var i = 0; i<titleName.length;i++)
+    {
+        titleName[i].id = i;
+        // 内容的显示与隐藏
+        titleName[i].onclick = function()
+        {
+            for( var j=0 ;j<titleName.length;j++)
+            {
+                outerDivs[j].style.display = "none";
+            }
+            outerDivs[this.id].style.display = "block";
+
+        }
+    }
+}
