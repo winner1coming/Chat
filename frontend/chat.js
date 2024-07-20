bd8aa2552b8c92c7e112bf81e0d066ecbcacc366
ma-xinjing <maxj29@mail2.sysu.edu.cn>
2024-07-20 18:51:53 +0800

    modified:   frontend/chat.js 	modified:   frontend/style.css 	modified:   frontend/wechat.html

--- frontend/chat.js
+++ frontend/chat.js
@@ -19,24 +19,35 @@ window.onload = function(){
             }
         }
     }
+    var current_id = ""
+    // 遍历获取当前右侧界面的id
+    for(var i=0;i<outerDivs.length;i++)
+    {
+
+    }
     // alert(outerDivs.length);
     //聊天界面切换
     for(var i = 0; i<titleName.length;i++)
-    {
-        titleName[i].id = i;
+    {   
         // 内容的显示与隐藏
         titleName[i].onclick = function()
         {
+            // alert(this.id);
             // 消除新消息气泡提示
             var b_label = titleName[this.id].getElementsByTagName("b")[0];
-            b_label.style.display = "none";
+            if(b_label)
+            {
+                b_label.style.display = "none";
+            }
+            // 将该list设置为亮色
+            
+
             // 显示对应的页面
             for( var j=0 ;j<titleName.length;j++)
             {
                 outerDivs[j].style.display = "none";
             }
             outerDivs[this.id].style.display = "block";
-
         }
     }
 }
