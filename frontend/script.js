0b4c4f505b7fe8e2399db7ee2f09bc28d2ed4dc8
AnliClark <le.clark9999@Gmail.com>
2024-07-20 14:11:10 +0800

    更新了用户列表

--- frontend/script.js
+++ frontend/script.js
@@ -28,7 +28,7 @@ const chatUserName = document.getElementById('chat_user_name');  // 对方的用
 // const chatUserStatus = document.getElementById('chat_user_status');  // 是否在线
 const chatUserImg = document.getElementById('chat_user_img');  // 对方的头像
 
-var chatHistory = new Array() // 保存聊天记录，{usersname(String): html标签}
+let chatHistory = new Array() // 保存聊天记录，{usersname(String): html标签}
 
 
 
@@ -42,47 +42,84 @@ ws.onmessage = function(event) {
     console.log('收到消息')  // todo: debug
     console.log(data)  // todo: debug
     if (data["type"] === 'private_message') {   // 接收到私聊
-        boxAddMessage(data["from"], data["message"], data["timestamp"]);
-    } else if (data["type"] === 'update_users') {  // 更新用户列表
-        console.log('即将更新用户列表')  // todo: debug
-        updateUsersList(data["users"]);
+        boxAddMessage(data["from"], currentUser, data["message"], data["timestamp"]);
+    }else if(data["type"] === 'add_user'){
+        addUser(data["user"]);
     }
+    // } else if (data["type"] === 'update_users') {  // 更新用户列表
+    //     console.log('即将更新用户列表')  // todo: debug
+    //     updateUsersList(data["users"]);
+    // }
 };
 
-// 更新用户列表
-function updateUsersList(users) {
-    console.log('更新用户列表')  // todo: debug
-    chatlist.innerHTML = '';
-    users.forEach(user => {
-        if (user !== currentUser) {
-            const userBlock = document.createElement('div');
-            userBlock.className = 'block active';
-            userBlock.innerHTML = `
-                <!-- 头像 -->
-                <div class="imgbx">
-                    <img src="img1.jpg" class="cover">
+// 增加新用户
+function addUser(user){
+    console.log('有新用户上线') 
+    if (user !== currentUser) {
+        const userBlock = document.createElement('div');
+        userBlock.className = 'block active';
+        userBlock.innerHTML = `
+            <!-- 头像 -->
+            <div class="imgbx">
+                <img src="img1.jpg" class="cover">
+            </div>
+            <div class="details">
+                <div class="listhead">
+                    <!-- 显示上线人员的网名 -->
+                    <h4>${user}</h4>
+                    <!-- 显示消息时间 -->
+                    <p class="time"></p>
                 </div>
-                <div class="details">
-                    <div class="listhead">
-                        <h4>${user}</h4>
-                    </div>
-                     <!-- 显示新收到的消息 -->
-                    <div class="message_p">
-                        <p></p>    <!--todo-->
-                    </div>
+                 <!-- 显示新收到的消息 -->
+                <div class="message_p">
+                    <p></p>    <!--内容-->
+                    <b></b>    <!--1表示为读-->
                 </div>
-            `;
-            userBlock.addEventListener('click', () => selectUser(user));
-            chatlist.appendChild(userBlock);
-        }
-    });
+            </div>
+        `;
+        userBlock.addEventListener('click', () => selectUser(user, userBlock));
+        chatlist.appendChild(userBlock);
+    }
 }
 
+// // 更新用户列表
+// function updateUsersList(users) {
+//     console.log('更新用户列表');
+//     chatlist.innerHTML = '';
+//     users.forEach(user => {
+//         if (user !== currentUser) {
+//             const userBlock = document.createElement('div');
+//             userBlock.className = 'block active';
+//             userBlock.innerHTML = `
+//                 <!-- 头像 -->
+//                 <div class="imgbx">
+//                     <img src="img1.jpg" class="cover">
+//                 </div>
+//                 <div class="details">
+//                     <div class="listhead">
+//                         <h4>${user}</h4>
+//                     </div>
+//                      <!-- 显示新收到的消息 -->
+//                     <div class="message_p">
+//                         <p></p>   
+//                         <b></b>    <!--1表示为读-->
+//                     </div>
+//                 </div>
+//             `;
+//             userBlock.addEventListener('click', () => selectUser(user, userBlock));
+//             chatlist.appendChild(userBlock);
+//         }
+//     });
+// }
+
 // 选择要聊天的用户
-function selectUser(user) {
-    // 保存与上一个用户聊天的记录
-    if (!currentChatUser){ 
-        chatHistory[currentChatUser] = chatBox.innerHTML;
+function selectUser(user, userBlock) {
+    // 保存与上一个用户聊天的记录（deleted，因为boxAddMessage里会直接保存历史）
+    // if (!currentChatUser){ 
+    //     chatHistory[currentChatUser] = chatBox.innerHTML;
+    // }
+    if(!currentChatUser){  //清除未读消息
+        userBlock.querySelector('.message_p').firstElementChild.nextElementSibling = ''
     }
     currentChatUser = user;
     chatUserName.innerText = user;  
@@ -97,16 +134,34 @@ function selectUser(user) {
 }
 
 // 在聊天箱里增加新消息
-function boxAddMessage(sendUser, message, timestamp) {  // sendUser为发送方
+function boxAddMessage(sendUser, receiveUser, message, timestamp) {  
     const messageDiv = document.createElement('div');
     messageDiv.className = `message ${currentUser === sendUser ? 'my_message' : 'friend_message'}`;
     messageDiv.innerHTML = `
         <p>${message}<br><span>${timestamp}</span></p>
     `;
+    if(sendUser === currentUser){
+        var peerUser = receiveUser;  // 对方
+    }else{
+        var peerUser = sendUser;  // 对方
+    }
     // 增加到对应的聊天历史里
-    chatHistory[currentChatUser].appendChild(messageDiv);
-    // 在用户列表里显示新消息
-
+    chatHistory[peerUser].appendChild(messageDiv);
+    // 在用户列表里显示新消息，
+    // 拿到所有的用户块（用户块下有子节点用户名和消息时间
+    //  ，其下一个兄弟节点的第一个子节点为消息，第二个表示是否未读，1表示为读）
+    let userlist = document.querySelectorAll(".listhead");  
+    var i=0;
+    for(;i<userlist.length;i++){
+        if(userlist[i].firstElementChild.innerText == peerUser){  // 要判断等于对方
+            userlist[i].firstElementChild.nextElementSibling.innerText = timestamp;  // 设置消息时间
+            userlist[i].nextElementSibling.firstElementChild.innerText = message;  // 设置消息内容
+            // 判断是否要显示未读
+            if(peerUser !== currentChatUser){
+                userlist[i].nextElementSibling.firstElementChild.innerHTML = 1;
+            }
+        }
+    }
     // 发送方即为当前的聊天方，在聊天箱里新增消息
     if(currentChatUser === sendUser){  
         chatBox.appendChild(messageDiv);
@@ -116,10 +171,10 @@ function boxAddMessage(sendUser, message, timestamp) {  // sendUser为发送方
 
 function sendMessage() {
     const message = messageInput.value;
-    if (!message !== '' && currentChatUser) {  // 判断message是否为空以及删去空格后是否为空，并且判断是否已经选了要发消息的对象
+    if (!message && currentChatUser) {  // 判断message是否为空以及删去空格后是否为空，并且判断是否已经选了要发消息的对象
         console.log('发送消息'+message);  // todo debug
         const timestamp = new Date().toLocaleTimeString();  // 获取时间戳
-        boxAddMessage(currentUser, message, timestamp);   // 在己方的对话框显示消息
+        boxAddMessage(currentUser, currentChatUser, message, timestamp);   // 在己方的对话框显示消息
         // 发送消息
         ws.send(JSON.stringify({ 
             "type": 'private_message',
